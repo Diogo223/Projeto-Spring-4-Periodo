28f31be3cbb0adf59469171ba793ef1e491e27bf
Diogo223 <48771284+Diogo223@users.noreply.github.com>
2019-11-20 08:47:30 -0300

    v10

--- src/main/java/com/example/demo/controller/ControlleCadastro.java
+++ src/main/java/com/example/demo/controller/ControlleCadastro.java
@@ -40,7 +40,7 @@ public class ControlleCadastro {
 			
 		crudRepository.save(professor);
 		attributes.addFlashAttribute("mensagen","Cadastrado com sucesso.");
-		return "/cadastro";
+		return "/cad";
 	}
 	
 	
