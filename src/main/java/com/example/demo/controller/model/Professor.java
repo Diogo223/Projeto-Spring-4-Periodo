c6dac83f1c703b553b9eb157dea307d58771b5f3
Diogo223 <48771284+Diogo223@users.noreply.github.com>
2019-11-25 16:05:13 -0300

    v15

--- src/main/java/com/example/demo/controller/model/Professor.java
+++ src/main/java/com/example/demo/controller/model/Professor.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 
 
@@ -12,16 +13,16 @@ import javax.validation.constraints.NotNull;
 public class Professor{	
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	
 	
 	private Long id;
 	
-	@NotNull(message = "data é obrigatório")
+	@NotBlank(message = "data é obrigatório")
 	@Column
 	private String data;
 	
-	@NotNull(message = "intervalo é obrigatório")
+	@NotBlank(message = "intervalo é obrigatório")
 	@Column
 	private String intervalo;
 	
